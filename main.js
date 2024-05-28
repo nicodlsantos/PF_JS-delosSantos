fc3b0aff441a15ac80ce1e17d03fecb684c11c90
Nicolás de los Santos <ndelossantosdaz@gmail.com>
2024-05-28 01:07:20 -0300

    Pre entregable 2: Se agrega estructura HTML y CSS. JS con ux mejorada en la agenda

--- main.js
+++ main.js
@@ -1,63 +1,52 @@
-const agenda = []
 
-function agendarCliente(nombre, apellido, mes, dia, hora) {
-    const clienteAgendado = { nombre: nombre, apellido: apellido, mes: mes, dia: dia, hora: hora }
-    agenda.push(clienteAgendado)
+const agenda = JSON.parse(localStorage.getItem('agenda')) || [];
+
+function mostrarAgendas() {
+    let agendasHTML = '';
+    agenda.forEach((agendado, index) => {
+        agendasHTML += `
+            <div class="agenda-item">
+                <p>Fecha: ${agendado.fecha}, Hora: ${agendado.hora}, Nombre: ${agendado.nombre}, Apellido: ${agendado.apellido}</p>
+                <button class="borrar-agenda-btn">Borrar</button>
+            </div>
+        `;
+    });
+
+    let containerAgendas = document.querySelector('.container-agendas');
+    containerAgendas.innerHTML = agendasHTML;
+
+    // Agrega event listener a cada botón de borrar agenda y actualiza localStorage
+    let botonesBorrar = document.querySelectorAll('.borrar-agenda-btn');
+    botonesBorrar.forEach((boton, index) => {
+        boton.addEventListener('click', function() {
+            agenda.splice(index, 1);
+
+            localStorage.setItem('agenda', JSON.stringify(agenda));
+
+            mostrarAgendas();
+        });
+    });
 }
 
-function verAgenda(nombre, apellido) {
-    for (const agendado of agenda) {
-        if (agendado.nombre == nombre && agendado.apellido == apellido) {
-            alert(agendado.nombre + ' ' + agendado.apellido + ' tu cita esta confirmada para el ' + agendado.dia + '/' + agendado.mes + ' a las ' + agendado.hora + ' hs.')
-        } else {
-            alert('Su nombre no figura en el sistema. De ser un error, intente nuevamente.')
-        }
-    }
-}
+// Función para agregar una nueva agenda
+document.getElementById("crear-agenda-btn").addEventListener("click", function() {
+    let fecha = document.getElementById("fecha").value;
+    let hora = document.getElementById("hora").value;
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
 
-function cancelarAgenda(nombre, apellido) {
-    for (let i = 0; i < agenda.length; i++) {
-        const agendado = agenda[i];
-        if (agendado.nombre === nombre && agendado.apellido === apellido) {
-            if (confirm(agendado.nombre + ' ' + agendado.apellido + ', ¿Quieres cancelar tu cita del día ' + agendado.dia + ' a las ' + agendado.hora + '?')) {
-                agenda.splice(i, 1);
-                alert('Cita cancelada correctamente.');
-            } else {
-                alert('No se encontró ninguna cita para cancelar.')
-            }
-        }
-    }
-}
+    let nuevaAgenda = {
+        fecha: fecha,
+        hora: hora,
+        nombre: nombre,
+        apellido: apellido
+    };
 
+    agenda.push(nuevaAgenda);
 
+    localStorage.setItem('agenda', JSON.stringify(agenda));
 
-function main() {
-    alert("Bienvenido")
-    let continuar = confirm("¿Desea realizar alguna accion?")
-    while (continuar) {
-        const accion = prompt("Ingrese la accion que desea realizar (Agendarme, Ver agenda, Cancelar agenda)")
-        if (accion == "Agendarme") {
-            const nombreIngresa = prompt("Ingrese su nombre")
-            const apellidoIngresa = prompt("Ingrese su apellido")
-            const mesIngresa = parseInt(prompt("Ingresa el mes en que desea agendarse (ej. 04)"))
-            const diaIngresa = parseInt(prompt("Ingresa el dia en que desea agendarse (ej. 14)"))
-            const horaIngresa = parseInt(prompt("Ingrese la hora en que desea agendarse (ej. 21)"))
-            agendarCliente(nombreIngresa, apellidoIngresa, mesIngresa, diaIngresa, horaIngresa)
-            verAgenda(nombreIngresa, apellidoIngresa)
-        } else if (accion == "Ver agenda") {
-            const nombreIngresa = prompt("Ingrese su nombre")
-            const apellidoIngresa = prompt("Ingrese su apellido")
-            verAgenda(nombreIngresa, apellidoIngresa)
-        } else if (accion == "Cancelar agenda") {
-            const nombreIngresa = prompt("Ingrese su nombre")
-            const apellidoIngresa = prompt("Ingrese su apellido")
-            cancelarAgenda(nombreIngresa, apellidoIngresa)
-        } else {
-            alert("Accion no valida")
-        }
-        continuar = confirm("¿Desea realizar alguna otra accion?")
-    }
+    mostrarAgendas();
+});
 
-}
-
-main()
+mostrarAgendas();
